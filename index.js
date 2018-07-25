f1e36b48c6fcaf7e9107b3d1254edd33db0d76be
n3m3sis42 <n3m3sis42@gmail.com>
2018-07-25 12:57:32 -0400

    Updated index.js and index.html with code for location search

--- index.js
+++ index.js
@@ -5,4 +5,27 @@ function initMap() {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
   });
+
+  var geocoder = new google.maps.Geocoder();
+
+  // NOTE: Add event listener for when the user clicks Search button
+  document.getElementById('submit').addEventListener('click', function() {
+    geocodeAddress(geocoder, map);
+  });
+}
+
+// NOTE: Add code to translate location in search field into latitude and longitude
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('address').value;
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === 'OK') {
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
 }
